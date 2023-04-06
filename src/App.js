eacb851f5527f6ce9232fcc47658bcf9c6842499
ElQuebeck <varelaemi@gmail.com>
2023-04-06 14:07:24 -0300

    Portfolio1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+'use strict'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+$(document).ready(function(){
+    $('body').on({ 'mousemove' : function(e){
+        console.clear()
+        let clientX = e.originalEvent.clientX
+        let clientY = e.originalEvent.clientY
+        $('#cursor').css({
+            'left' : (clientX - 40) + 'px',
+            'top' : (clientY - 40) + 'px'
+        })
+    }})
 
-export default App;
+    $('.a').on({
+        'mouseover' : function () {
+            $('#cursor').addClass('mini')
+        },
+        'mouseout' : function () {
+            $('#cursor').removeClass('mini')
+        }
+    })
+})
